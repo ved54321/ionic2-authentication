768a88e1ef2342a30bae52f6a82a7e05f5517c80
Vedprakash Singh <vedprakash.singh@arrkgroup.com>
2017-08-13 00:08:24 +0530

    Added student voice page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
@@ -10,36 +11,17 @@ export class ListPage {
   selectedItem: any;
   icons: string[];
   studentvoices: studentvoice[];
-  items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public authService: AuthServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public authService: AuthServiceProvider,public loadingCtrl: LoadingController) {
      this.authService.getstudentvoice().subscribe(studentvoices =>{
-            this.studentvoices = studentvoices;
-    });
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+        let loader = this.loadingCtrl.create({
+          content: "Please wait..." 
+        });
+        loader.present();
+        this.studentvoices = studentvoices;
+        loader.dismiss();
+    }); 
   }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
-  }
-
 }
 
 interface studentvoice{
